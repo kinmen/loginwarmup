002b8e459bd46289f58b9ace391e7c73aa019b79
kinmen <sparkstar6142@berkeley.edu>
2014-09-25 22:31:29 -0700

    moe tests

--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 var User = require('./model/model.js')
 var assert = require("assert")
+require('./app.js')
 
 describe('isvalidname', function() {
 	it("should be valid user", function(done){
@@ -28,5 +29,43 @@ describe('isvalidpass', function() {
 	it("too long pw", function(done){
 		assert(User.isValidPassword("a"*130))
 		done()
+	})
+})
+describe('users', function() {
+	it("should be valid schema", function(done) {
+		var userName = 'asdf'
+		var passWord = 'peedubya'
+		var newuser = new User({user: userName, password: passWord, count: 1});
+		assert(newuser.user == userName)
+		assert(newuser.password == passWord)
+		assert(newuser.count == 1)
+		done()
+	})
+})
+describe('userdbstuff', function() {
+	var userName = 'a'
+	var passWord = 'peedubya'
+	before(function(done) {
+		User.remove({},function(err) {
+			done()
+		})
+	})
+	describe('users2', function() {
+		it("should add and find", function(done) {
+			var newuser = new User({user: userName, password: passWord, count: 1});
+			newuser.save(done);
+		})
+		it("should find", function(done) {
+			User.findOne({user: userName}, function(err, found){
+				assert(found.user == userName)
+				done()
+			})
+		})
+		it('should find and inc count', function(done) {
+			User.findOneAndUpdate({user:userName}, {$inc: {count:1}}, function(err,found) {
+				assert(found.count == 2)
+				done()
+			})
+		})
 	})
 })
