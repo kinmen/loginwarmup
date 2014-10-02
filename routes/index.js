3b593d2e9c963499a86cdfa58e46085068815ba6
kinmen <sparkstar6142@berkeley.edu>
2014-10-01 17:37:24 -0700

    Implemented views with css/ couple route changes

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Please enter your credentials below' });
 });
 
 router.get('/users/login', function(req, res) {
@@ -16,55 +16,47 @@ router.get('/users/login', function(req, res) {
 })
 
 router.post('/users/login', function(req, res) {
-	var db = req.db;
 
 	var userName = req.body.user;
 	var passWord = req.body.password;
 
 	User.findOneAndUpdate({user: userName, password: passWord}, {$inc: {count: 1}}, function(err, docs) {
 		if (!docs) {
-			return res.json({errCode: -1});
-			//return res.render('index', {title: "Invalid username and password combination. Please try again."});
+			//return res.json({errCode: -1});
+			return res.render('index', {title: "Invalid username and password combination. Please try again."});
 		} else {
-			//res.render('success', {count: docs.count});
-			return res.json({errCode: 1, count: docs.count})
+			var welcome = "Welcome " + userName + ". You have logged in " + docs.count + " times."
+			return res.render('success', {title: welcome, count: docs.count});
+			// return res.json({errCode: 1, count: docs.count})
 		}
 	})
 })
-	// collection.findAndModify(newuser, function(err, object) {
-	// 	if (err) {
-	// 		res.send("Error adding to database");
-	// 	} else {
-	// 		res.send("added" + userName);
-	// 	}
-	// })
-router.get('/users/add', function(req, res) {
-	res.render('index', { title: 'Please enter your credentials below'});
-})
+
 router.post('/users/add', function(req, res) {
-	//var db = req.db;
 
 	var userName = req.body.user;
 	var passWord = req.body.password;
 
 	if (!User.isValidUser(userName)) {
-		return res.json({errCode: -3});
-		// return res.render('index', {title : "The user name should be non-empty and at most 128 characters long. Please try again."})
+		//return res.json({errCode: -3});
+		return res.render('index', {title : "The user name should be non-empty and at most 128 characters long. Please try again."})
 	}
 	if (!User.isValidPassword(passWord)) {
-		return res.json({errCode : -4});
-		// return res.render('index', {title : "The password should be at most 128 characters long. Please try again."})
+		// return res.json({errCode : -4});
+		return res.render('index', {title : "The password should be at most 128 characters long. Please try again."})
 	}
 
 	var newuser = User({user: userName, password: passWord, count: 1});
 
 	User.findOne({user: userName}, function(err, found) {
 		if(found) {
-			return res.json({errCode: -2})
-			//return res.render('index', {title : "This username already exists. Please try again."});
+			// return res.json({errCode: -2})
+			return res.render('index', {title : "This username already exists. Please try again."});
 		} else {
 			newuser.save(function(err, newuser) {
-				return res.json({errCode: 1, count : 1})
+				// return res.json({errCode: 1, count : 1})
+				var welcome = "Welcome " + userName + ". You have logged in " + newuser.count + " times."
+				return res.render('success', {title : welcome, count: newuser.count});
 			})
 		}
 	})
